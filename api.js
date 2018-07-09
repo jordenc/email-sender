993c0e4e9664cf31f457ad09078f921cac9d1704
Jorden <jchamid@totallyhosted.nl>
2018-07-09 11:45:19 +0200

    Support for API sendmail

--- api.js
+++ api.js
@@ -58,5 +58,78 @@ module.exports = [
 			});
 			
         }
+    },
+    /* Required GET input: mailto (e-mail address), subject, body (message) */
+    {
+	    
+	    description:		'API Send Email',
+        method: 		'GET',
+        path:			'/sendmail/',
+        fn: function(args, callback){
+	        
+	        var nodemailer = require('nodemailer');
+			
+			mail_user = Homey.ManagerSettings.get('mail_user');
+			mail_pass = Homey.ManagerSettings.get('mail_password');
+			mail_host = Homey.ManagerSettings.get('mail_host');
+			mail_port = Homey.ManagerSettings.get('mail_port');
+			mail_from = Homey.ManagerSettings.get('mail_from');
+			mail_secure = Homey.ManagerSettings.get('mail_secure');
+		
+			if ( typeof mail_user !== 'undefined' && typeof mail_pass !== 'undefined' && typeof mail_host !== 'undefined' && typeof mail_port !== 'undefined' && typeof mail_from !== 'undefined') {
+
+				if (typeof use_credentials == undefined) use_credentials = true;
+	
+				if (use_credentials) {
+					var transporter = nodemailer.createTransport(
+					{
+						host: mail_host,
+						port: mail_port,
+						secure: mail_secure,
+						auth: {
+							user: mail_user,
+							pass: mail_pass
+						},
+						tls: {rejectUnauthorized: false}
+					});
+				} else {
+					// Don't use authentication. Not supported by all providers
+					var transporter = nodemailer.createTransport(
+					{
+						host: mail_host,
+						port: mail_port,
+						secure: mail_secure,
+						tls: {rejectUnauthorized: false}
+					});
+				}
+	
+			    var mailOptions = {
+	
+					from: 'Homey <' + mail_from + '>',
+				    to: args.query.mailto,
+				    subject: args.query.subject,
+				    text: args.query.body,
+					  html: args.query.body
+			    }
+	
+			    transporter.sendMail(mailOptions, function(error, info){
+				    if(error){
+					    return this.error(error);
+				    }
+				    console.log('Message sent: ' + info.response);
+				    return Promise.resolve (true);
+				    
+				});
+	
+			} else {
+	
+				this.log('Not all required variables for mailing have been set');
+	
+				callback ('Not all required variables for mailing have been set', false);
+	
+			}
+	    
+	    }
+	    
     }
 ]
